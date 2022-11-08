fc284ceb0d69810068b6115d9ad296e72d3ea814
Ansh <63942964+Ansh-dev-2005@users.noreply.github.com>
2022-11-08 19:13:55 +0530

    update

--- Activity6/1st_functions.c
+++ Activity6/1st_functions.c
@@ -15,12 +15,38 @@ while(temp!=0) {
     else 
     printf("The Number is not an Armstrong Number\n"); 
 }
+void coprime(int n){
+    int a=n, reverse = 0, remainder;
+
+  while (a != 0) {
+    remainder = a % 10;
+    reverse = reverse * 10 + remainder;
+    a /= 10;
+  }
+    if(n==reverse){
+        printf("Enter number's reverse is equal to the number\n");
+    }else{
+        printf("Not equal to its reverse\n");
+    }
+}
+int factorial(int n){
+int i,fact=1,number=n;    
+     
+    for(i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  printf("Factorial of %d is: %d",number,fact);    
+return 0;  
+} 
+
 int main()
 {
     int n;
     printf("Enter the number");
     scanf("%d",&n);
     Armstrong(n);
+    coprime(n);
+    factorial(n);
 
 
     return 0;
