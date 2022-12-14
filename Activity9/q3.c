4c77f898a6bac0800dd418f7c40583e214a6ed3e
Ansh <63942964+Ansh-dev-2005@users.noreply.github.com>
2022-12-14 11:25:34 +0530

    updated expermient 9th 3 question

--- Activity9/q3.c
+++ Activity9/q3.c
@@ -1,38 +1,21 @@
-//Design a union ‘product’ to store the details of the product purchased like product name, price per unit,number of quantities purchased, and amount spent. Get the name, price per unit, and number of quantitiesof the product purchased. Calculate the amount spent on the product and then display all the details of theprocured product using union pointers.
 #include<stdio.h>
-#include<stdlib.h>
-union product
-{
-    char name[20];
-    int quantity;
-    float price;
-    float amount;
-};
-int main()
-{
-    union product *ptr;
-    int n, i;
-    printf("Enter the number of products purchased: ");
-    scanf("%d", &n);
-    ptr = (union product *)malloc(n * sizeof(union product));
-    for (i = 0; i < n; i++)
-    {
-        printf("Enter the name of the product:  ");
-        scanf("%s", (ptr + i)->name);
-        printf("Enter the quantity of the product: ");
-        scanf("%d", &(ptr + i)->quantity);
-        printf("Enter the price of the product: ");
-        scanf("%f", &(ptr + i)->price);
-        (ptr + i)->amount = (ptr + i)->quantity * (ptr + i)->price;
-    }
-    printf("Details of the products purchased:");
-    for (i = 0; i < n; i++)
-    {
-        printf("Name: %s \n", (ptr + i)->name);
-        printf("Quantity: %d \n", (ptr + i)->quantity);
-        printf("Price: %f \n", (ptr + i)->price);
-        printf("Amount: %f \n", (ptr + i)->amount);
-    }
-    free(ptr);
-    return 0;
-}
+union product{
+    char name[100];
+    float m;
+    int n;
+    float o;
+    };
+    int main()
+    {int x, y;
+    union product p;
+    printf("Enter Product Name: ");
+    scanf("%s",p.name);
+    printf("Enter Price per unit: ");
+    scanf("%f", &p.m);
+    x= p.m;
+    printf("Enter Quantity: ");
+    scanf("%d", &p.n);
+    y=p.n;
+    p.o=x*y;
+    printf("Total Amount= %f",p.o);
+    return 0;}
